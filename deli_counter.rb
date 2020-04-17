40195d512b6b775cfab9d73413f7eeaa28e1f4bc
Michael Piercy <michael.piercy@zoho.com>
2020-04-17 22:09:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,18 +11,3 @@ def line(x)
     puts "The line is currently: #{line_array.join(" ")}"
   end
 end
-
-katz_deli = []
-
-def line(cust)
-  line_array = []
-  if cust.length == 0
-  puts "The line is currently empty."
-  else
-  cust.each.with.index(1) do |name, position|
-  line_array.push("#{position}. #{name}")
-  end
-  puts "The line is currently: #{line_array.join(" ")}"
-  end
-end
-
