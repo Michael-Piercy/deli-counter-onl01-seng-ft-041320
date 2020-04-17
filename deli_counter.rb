80ff2b55b37497f039279eea3069158cfa82fcb7
Michael Piercy <michael.piercy@zoho.com>
2020-04-17 22:22:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,4 +17,11 @@ def take_a_number(katz_deli, cust)
   puts "Welcome, #{cust}. You are number #{katz_deli.length} in line."
 end
 
-
+def now_serving(cust)
+  if cust.empty?
+  puts "There is nobody waiting to be served!"
+  else
+  puts "Currently serving #{cust[0]}."
+  cust.shift
+  end
+end
